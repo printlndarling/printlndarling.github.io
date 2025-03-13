048741cdaeebb171094cb9450ff49d484e107e6e
繁星_逐梦 <printlndarling@qq.com>
2025-03-13 12:08:37 +0800

    Site updated: 2025-03-13 12:08:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/12/hello-world/"];function toRandomPost(){
+var posts=["posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
